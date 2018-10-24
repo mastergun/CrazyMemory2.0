dd1ed407ced0d23008347b55e492150ec47f07ab
mastergun <martimendrugu@gmail.com>
2018-10-24 02:59:08 +0200

    start to implement a HUD code and interaction

---
+++ Assets/Scripts/InterfaceController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InterfaceController : MonoBehaviour {
+
+    public enum MENUTYPE
+    {
+        MAINMENU,
+        GALERYMENU,
+        TEAMMENU,
+        SETTINGSMENU,
+        GAMEMENU,
+        PAUSEMENU
+    }
+
+    //[System.Serializable]
+    //public struct Menu{
+    //    public GameObject menu;
+    //    public Image Background;
+    //    public MENUTYPE type;
+    //    public List<Button> buttons;
+    //}
+
+    public List<GameObject> menus;
+    
+	// Use this for initialization
+	void Start () {
+        SetMainMenu();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetMainMenu()
+    {
+        menus[(int)MENUTYPE.MAINMENU].SetActive(true);
+        menus[(int)MENUTYPE.GALERYMENU].SetActive(false);
+        menus[(int)MENUTYPE.TEAMMENU].SetActive(false);
+        menus[(int)MENUTYPE.SETTINGSMENU].SetActive(false);
+        GetComponent<GameController>().ResetGame();
+    }
+
+    public void SetGalleryMenu()
+    {
+        menus[(int)MENUTYPE.MAINMENU].SetActive(false);
+        menus[(int)MENUTYPE.GALERYMENU].SetActive(true);
+        menus[(int)MENUTYPE.TEAMMENU].SetActive(false);
+        menus[(int)MENUTYPE.SETTINGSMENU].SetActive(false);
+        //GetComponent<GameController>().ResetGame();
+    }
+
+    public void SetTeamMenu()
+    {
+        menus[(int)MENUTYPE.MAINMENU].SetActive(false);
+        menus[(int)MENUTYPE.GALERYMENU].SetActive(false);
+        menus[(int)MENUTYPE.TEAMMENU].SetActive(true);
+        menus[(int)MENUTYPE.SETTINGSMENU].SetActive(false);
+        //GetComponent<GameController>().ResetGame();
+    }
+
+    public void SetSettingsMenu()
+    {
+        menus[(int)MENUTYPE.MAINMENU].SetActive(false);
+        menus[(int)MENUTYPE.GALERYMENU].SetActive(false);
+        menus[(int)MENUTYPE.TEAMMENU].SetActive(false);
+        menus[(int)MENUTYPE.SETTINGSMENU].SetActive(true);
+        //GetComponent<GameController>().ResetGame();
+    }
+}
---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour {
+
+    enum BUTTONTYPE
+    {
+        PLAYBUTTON,
+        LOWDIFICULTBUTTON,
+        UPDIFICULTBUTTON,
+        EXITBUTTON,
+        GALERYBUTTON,
+        TEAMBUTTON,
+        SETTINGSBUTTON
+    }
+    public Image bgRef;
+    public Image difImRef;
+    public Text dificultTextRef;
+    public Text maxScoreTextRef;
+    public List<Color> bgDifColors;
+    public List<string> difTittles;
+    public List<Button> buttons;
+
+    List<float> maxScores; 
+    int DifficultSelected = 1;
+    // Use this for initialization
+    void Start () {
+        maxScores = new List<float>();
+        if(maxScores.Count == 0)
+        {
+            for(int i=0;i< 4; i++)
+            {
+                maxScores.Add(Random.Range(0,200.0f));
+            }
+        }
+        SetDifficult(-1);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetDifficult(int dificult)
+    {
+        if (dificult < 0 && DifficultSelected > 0) DifficultSelected--;
+        else if (dificult > 0 && DifficultSelected < 3) DifficultSelected++;
+        else
+        {
+            Debug.Log("impossible to change dificult");
+            return;
+        }
+        bgRef.color = bgDifColors[DifficultSelected];
+        dificultTextRef.text = difTittles[DifficultSelected];
+        maxScoreTextRef.text = maxScores[DifficultSelected].ToString();
+    }
+
+    public void SetMaxScoreTexts(int dif, float maxScore)
+    {
+        maxScores[dif] = maxScore;
+    }
+}
